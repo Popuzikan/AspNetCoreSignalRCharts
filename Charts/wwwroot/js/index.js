48ff1290e5c5f308538f488ea81041d8ebfd3fff
popuzikan <popuzikan@mail.ru>
2024-01-07 19:37:56 +0300

    OlegFix

--- Charts/wwwroot/js/index.js
+++ Charts/wwwroot/js/index.js
@@ -1,95 +1,21 @@
 const data = JSON.parse(document.getElementById('data').innerHTML);
 
 
-var corrGraph = new CorrelationGraph(document.getElementById('corrWaveForm'));
+var corrGraph = new WaveForm(document.getElementById('corrWaveForm'));
 
 const signalHubConnection = new signalR.HubConnectionBuilder()
     .withUrl(data.url)
     .configureLogging(signalR.LogLevel.Information)
     .build();
 
-signalHubConnection.on("addChartData", (data) => corrGraph.extendTraces([[data], [data]]));
+signalHubConnection.on("addChartData", (data) => {
+    console.log(data);
+    corrGraph.extendTraces([data.x]);
+});
 
 signalHubConnection.start();
 
+function pad(num, size) {
+    return ('000000000' + num).substr(-size);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//const data = JSON.parse(document.getElementById('data').innerHTML);
-//const ctx = document.getElementById('myChart').getContext('2d');
-
-
-//const myChart = new Chart(ctx, {
-//    type: 'line',
-//    data: data,
-//    options : {
-//        scales: {
-//            y : {
-//                suggestedMax: 100,
-//                suggestedMin: 1
-//            }
-//        }
-//    }
-//});
-
-//const connection = new signalR.HubConnectionBuilder()
-//    .withUrl(data.url)
-//    .configureLogging(signalR.LogLevel.Information)
-//    .build();
-
-//async function start() {
-//    try {
-//        await connection.start();
-//        console.log("SignalR Connected.");
-//    } catch (err) {
-//        console.log(err);
-//        setTimeout(start, 5000);
-//    }
-//}
-
-//connection.onclose(async () => {
-//    await start();
-//});
-
-//connection.on("addChartData", function (point) {
- 
-//    myChart.data.datasets.forEach((dataset) => {
-
-//        dataset.data.push([point.value]);
-//        myChart.data.labels.push([point.labels]);
-//    });
-
-//    myChart.update();
-
-//    if (myChart.data.labels.length > data.limit) {
-//        myChart.data.labels.splice(0, 350);
-//        myChart.data.datasets.forEach((dataset) => {
-//            dataset.data.splice(0, 350);
-//        });
-//        myChart.update();
-//    }
-//});
-
-// Start the connection.
-/*start().then(() => {});*/
--- Charts/wwwroot/js/waveForm.js
+++ Charts/wwwroot/js/waveForm.js
@@ -1,11 +1,69 @@
-////var waveFormGraph = new WaveForm(document.getElementById('graphWaveForm'));
-////var moduleWaveFormGraph = new WaveForm(document.getElementById('moduleWaveForm'));
+function WaveForm(element) {
+    var _element = element;
+    var _tracesCount;
+    var _tracesIndexes;
+    var _xValues;
 
-var corrGraph = new CorrelationGraph(document.getElementById('corrWaveForm'));
+    init(_element);
 
-const signalHubConnection = new signalR.HubConnectionBuilder()
-    .withUrl("/signal")
-    .build();
+    function init(element) {
+        var secondsAccumulationTime = 0.02;
+        var SamplingRate = 350;
+        _xValues = Array(100).fill().map((_, i) => i / SamplingRate * 1000);
 
-signalHubConnection.on("CorrelationDetected", (data) => corrGraph.extendTraces([[data.correlationMax], [data.threshold]]));
-signalHubConnection.start();
+        var layout = {
+            title: 'Развертка по времени',
+            yaxis: {
+                //range: [MIN_Y, MAX_Y],
+                autorange: true,
+                title: {
+                    text: 'Амплитуда',
+                }
+            },
+            xaxis: {
+                title: {
+                    text: 'Время, мс'
+                },
+            }
+        };
+
+        Plotly.react(element, [], layout, { responsive: true });
+    }
+
+    function extendTraces(traces) {
+        if (_tracesCount != traces.length) {
+            resetTraces(traces.length);
+        }
+
+        var extend = {};
+        extend.y = traces;
+
+        Plotly.extendTraces(_element, extend, _tracesIndexes, _xValues.length);
+    }
+
+    function resetTraces(newTracesCount) {
+        console.log(newTracesCount);
+        _element.data = [];
+        _tracesCount = newTracesCount;
+
+        _tracesIndexes = Array(_tracesCount)
+            .fill()
+            .map(function (_, i) {
+                return i
+            });
+
+        var traces = Array(_tracesCount).fill().map(function (_, i) {
+            return {
+                y: [],
+                x: _xValues,
+                mode: 'lines',
+                name: 'Канал ' + (i + 1)
+            };
+        })
+        Plotly.addTraces(_element, traces);
+    }
+
+    return {
+        extendTraces: extendTraces
+    }
+}
